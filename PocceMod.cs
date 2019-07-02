3fe169c9fbd050b3e610ef1bc219940be2f79aac
Razzie <gabor@gorzsony.com>
2019-07-02 22:53:09 +0200

    Initial commit

---
+++ PocceMod.cs
@@ -0,0 +1,348 @@
+﻿using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using MenuAPI;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PocceMod
+{
+    public sealed class PocceMod : PocceModBase
+    {
+        private Menu skinMenu;
+
+        public PocceMod()
+        {
+            SetupMenu();
+
+            Tick += OnTick;
+        }
+
+        public static async Task SpawnTrashPed()
+        {
+            var ped = await SpawnPed(TrashPedList);
+            API.StartEntityFire(ped);
+            API.SetEntityAsNoLongerNeeded(ref ped);
+        }
+
+        public static async Task PedRiot(bool useWeapons)
+        {
+            int i = 0;
+            var peds = GetPeds();
+            var weapons = useWeapons ? WeaponList : null;
+
+            if (peds.Count < 2)
+                return;
+
+            foreach (int ped in peds)
+            {
+                if (API.IsPedInAnyVehicle(ped, false))
+                {
+                    var vehicle = API.GetVehiclePedIsIn(ped, false);
+                    API.TaskLeaveVehicle(ped, vehicle, 1);
+                    await Delay(1000);
+                }
+
+                API.ClearPedTasks(ped);
+
+                await ArmPed(ped, weapons);
+
+                int enemyPed;
+                if (i % 2 == 0)
+                    enemyPed = peds[(i + 1) % peds.Count];
+                else if (i == peds.Count - 1)
+                    enemyPed = peds[0];
+                else
+                    enemyPed = peds[i - 1];
+                API.TaskCombatPed(ped, enemyPed, 0, 16);
+
+                int tmp_ped = ped; API.SetEntityAsNoLongerNeeded(ref tmp_ped);
+
+                ++i;
+            }
+        }
+
+        public static async Task PocceRiot(bool useWeapons)
+        {
+            var peds = new List<int>();
+            var weapons = useWeapons ? WeaponList : null;
+
+            for (int i = 0; i < 4; ++i)
+            {
+                int ped1 = await SpawnPed(PocceList);
+                int ped2 = await SpawnPed(PocceList);
+
+                peds.Add(ped1);
+                peds.Add(ped2);
+
+                await ArmPed(ped1, weapons);
+                await ArmPed(ped2, weapons);
+
+                API.TaskCombatPed(ped1, ped2, 0, 16);
+                API.TaskCombatPed(ped2, ped1, 0, 16);
+            }
+
+            for (int i = 0; i < 4; ++i)
+            {
+                int ped = await SpawnPed(PocceList);
+                peds.Add(ped);
+                await ArmPed(ped, weapons);
+                API.TaskCombatPed(ped, Game.Player.Character.Handle, 0, 16);
+            }
+
+            foreach (int ped in peds)
+            {
+                int tmp_ped = ped;
+                API.SetEntityAsNoLongerNeeded(ref tmp_ped);
+            }
+        }
+
+        public static async Task PoccePassengers()
+        {
+            int player = Game.Player.Character.Handle;
+
+            if (!API.IsPedInAnyVehicle(player, true))
+            {
+                Notification("Player is not in a vehicle");
+                return;
+            }
+
+            var vehicle = API.GetVehiclePedIsIn(player, false);
+            var vehicleModel = API.GetEntityModel(vehicle);
+            var seats = API.GetVehicleModelNumberOfSeats((uint)vehicleModel);
+
+            for (int seat = -1; seat < seats; ++seat)
+            {
+                if (API.IsVehicleSeatFree(vehicle, seat))
+                {
+                    var pocce = PocceList[API.GetRandomIntInRange(0, PocceList.Length)];
+                    await RequestModel(pocce);
+                    var ped = API.CreatePedInsideVehicle(vehicle, 26, pocce, seat, true, false);
+                    API.SetEntityAsNoLongerNeeded(ref ped);
+                }
+            }
+        }
+
+        public static async Task PocceCompanion()
+        {
+            var ped = await SpawnPed(PocceList);
+            await MakeCompanion(ped);
+            await ArmPed(ped, WeaponList);
+            API.SetEntityAsNoLongerNeeded(ref ped);
+        }
+
+        public static async Task PetCompanion()
+        {
+            var ped = await SpawnPed(PetList, 28);
+            await MakeCompanion(ped);
+            await ArmPed(ped, null);
+            API.SetEntityAsNoLongerNeeded(ref ped);
+        }
+
+        public static List<string> IdentifyPedModels()
+        {
+            var coords = Game.Player.Character.Position;
+            var peds = GetPeds(true, true);
+            var models = new List<string>();
+
+            foreach (var ped in peds)
+            {
+                var pos = new Ped(ped).Position;
+                if (coords.DistanceToSquared(pos) < 4.0f)
+                {
+                    var model = string.Format("0x{0:X8}", API.GetEntityModel(ped));
+                    models.Add(model);
+                    Notification("ped:" + model);
+                }
+            }
+
+            return models;
+        }
+
+        private void AddSkins(List<string> models)
+        {
+            foreach (var model in models)
+            {
+                bool newSkin = true;
+                var skins = skinMenu.GetMenuItems();
+                foreach (var skin in skins)
+                {
+                    if (skin.Text == model)
+                    {
+                        newSkin = false;
+                        break;
+                    }
+                }
+                if (newSkin)
+                {
+                    skinMenu.AddMenuItem(new MenuItem(model));
+                }
+            }
+        }
+
+        private void SetupMenu()
+        {
+            MenuController.MenuToggleKey = Control.SelectCharacterMichael;
+            MenuController.EnableMenuToggleKeyOnController = false;
+            MenuController.MenuAlignment = MenuController.MenuAlignmentOption.Right;
+
+            Menu menu = new Menu("PocceMod", "menu");
+            MenuController.AddMenu(menu);
+
+            var spawnVehicleButton = new MenuItem("Spawn vehicle");
+            menu.AddMenuItem(spawnVehicleButton);
+            var spawnPropButton = new MenuItem("Spawn prop");
+            menu.AddMenuItem(spawnPropButton);
+            menu.AddMenuItem(new MenuItem("Spawn trash ped"));
+            menu.AddMenuItem(new MenuItem("Ped riot"));
+            menu.AddMenuItem(new MenuItem("Ped riot with weapons"));
+            menu.AddMenuItem(new MenuItem("Pocce riot"));
+            menu.AddMenuItem(new MenuItem("Pocce riot with weapons"));
+            menu.AddMenuItem(new MenuItem("Pocce passengers"));
+            menu.AddMenuItem(new MenuItem("Pocce companion"));
+            menu.AddMenuItem(new MenuItem("Pet companion"));
+            menu.AddMenuItem(new MenuItem("Identify skins"));
+            var skinMenuButton = new MenuItem("Change skin");
+            menu.AddMenuItem(skinMenuButton);
+
+            menu.OnItemSelect += async (_menu, _item, _index) =>
+            {
+                switch (_index)
+                {
+                    // submenus
+                    case 0:
+                    case 1:
+                    case 11:
+                        return;
+
+                    case 2:
+                        await SpawnTrashPed();
+                        break;
+
+                    case 3:
+                        await PedRiot(false);
+                        break;
+
+                    case 4:
+                        await PedRiot(true);
+                        break;
+
+                    case 5:
+                        await PocceRiot(false);
+                        break;
+
+                    case 6:
+                        await PocceRiot(true);
+                        break;
+
+                    case 7:
+                        await PoccePassengers();
+                        break;
+
+                    case 8:
+                        await PocceCompanion();
+                        break;
+
+                    case 9:
+                        await PetCompanion();
+                        break;
+
+                    case 10:
+                        var models = IdentifyPedModels();
+                        AddSkins(models);
+                        break;
+                }
+
+                menu.CloseMenu();
+            };
+
+
+            Menu vehicleMenu = new Menu("PocceMod", "select vehicle");
+            MenuController.AddSubmenu(menu, vehicleMenu);
+            MenuController.BindMenuItem(menu, vehicleMenu, spawnVehicleButton);
+
+            string vehicles = API.LoadResourceFile(API.GetCurrentResourceName(), "vehicles.ini");
+            foreach (var vehicle in vehicles.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                vehicleMenu.AddMenuItem(new MenuItem(vehicle));
+            }
+
+            vehicleMenu.OnItemSelect += async (_menu, _item, _index) =>
+            {
+                var vehicle = await SpawnVehicle(_item.Text);
+                API.SetEntityAsNoLongerNeeded(ref vehicle);
+                vehicleMenu.CloseMenu();
+            };
+
+
+            Menu propMenu = new Menu("PocceMod", "select prop");
+            MenuController.AddSubmenu(menu, propMenu);
+            MenuController.BindMenuItem(menu, propMenu, spawnPropButton);
+
+            var propList = new List<string>();
+            string lastPropPrefix = string.Empty;
+            string props = API.LoadResourceFile(API.GetCurrentResourceName(), "props.ini");
+            foreach (var prop in props.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                var propPrefix = prop.Substring(0, 10);
+                if (propPrefix != lastPropPrefix)
+                {
+                    if (propList.Count > 0)
+                    {
+                        if (propList.Count == 1)
+                        {
+                            var propItem = new MenuItem(propList[0]);
+                            propMenu.AddMenuItem(propItem);
+                        }
+                        else
+                        {
+                            var propListItem = new MenuListItem(lastPropPrefix + "*", propList, 0);
+                            propMenu.AddMenuItem(propListItem);
+                        }
+                        propList = new List<string>();
+                    }
+                    lastPropPrefix = propPrefix;
+                }
+
+                propList.Add(prop);
+            }
+            if (propList.Count > 0)
+            {
+                var propListItem = new MenuListItem(lastPropPrefix, propList, 0);
+                propMenu.AddMenuItem(propListItem);
+            }
+
+            propMenu.OnItemSelect += async (_menu, _item, _index) =>
+            {
+                var prop = await SpawnProp(_item.Text);
+                API.SetEntityAsNoLongerNeeded(ref prop);
+                propMenu.CloseMenu();
+            };
+
+            propMenu.OnListItemSelect += async (_menu, _listItem, _listIndex, _itemIndex) =>
+            {
+                var prop = await SpawnProp(_listItem.ListItems[_listIndex]);
+                API.SetEntityAsNoLongerNeeded(ref prop);
+                propMenu.CloseMenu();
+            };
+
+
+            skinMenu = new Menu("PocceMod", "identified skins");
+            MenuController.AddSubmenu(menu, skinMenu);
+            MenuController.BindMenuItem(menu, skinMenu, skinMenuButton);
+
+            skinMenu.OnItemSelect += async (_menu, _item, _index) =>
+            {
+                var model = uint.Parse(_item.Text.Substring(2), System.Globalization.NumberStyles.HexNumber);
+                await Game.Player.ChangeModel(new Model((PedHash)model));
+                skinMenu.CloseMenu();
+            };
+        }
+
+        private async Task OnTick()
+        {
+            await Delay(2000);
+            await UpdateCompanions();
+        }
+    }
+}
---
+++ PocceModBase.cs
@@ -0,0 +1,323 @@
+﻿using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PocceMod
+{
+    public abstract class PocceModBase : BaseScript
+    {
+        public static uint[] WeaponList { get; } = { 0x1B06D571, 0xBFEFFF6D, 0x1D073A89, 0x958A4A8F, 0x3656C8C1 };
+        public static uint[] TrashPedList { get; } = { 0xE16D8F01, 0x6BD9B68C, 0xB097523B, 0x0E32D8D0, 0xE0E69974 };
+        public static uint[] PocceList { get; } = { 0x780C01BD, 0x303638A7, 0xC79F6928, 0x445AC854, 0x028ABF95, 0x9FC7F637, 0x9CF26183, 0xBE086EFD, 0xDB134533, 0x5AA42C21, 0xC7496729, 0x5C2CF7F8, 0x20C8012F };
+        public static uint[] PetList { get; } = { 0x573201B8, 0x14EC17EA, 0x4E8F95A2, 0x6D362854, 0x9563221D, 0x431FC24C, 0xA8683715 };
+        public static string[] ScenarioList { get; } = { "WORLD_HUMAN_AA_COFFEE", "WORLD_HUMAN_AA_SMOKE", "WORLD_HUMAN_DRINKING", "WORLD_HUMAN_PARTYING", "WORLD_HUMAN_PUSH_UPS", "WORLD_HUMAN_SUPERHERO" };
+        public static string PocceCompanionDecor = "POCCE_COMPANION";
+
+        public static void Notification(string message, bool blink = false, bool saveToBrief = false)
+        {
+            API.SetNotificationTextEntry("CELL_EMAIL_BCON");
+            foreach (string s in CitizenFX.Core.UI.Screen.StringToArray(message))
+            {
+                API.AddTextComponentSubstringPlayerName(s);
+            }
+            API.DrawNotification(blink, saveToBrief);
+        }
+
+        public static List<int> GetPeds(bool includeAnimals = true, bool includePlayers = false, bool includeDead = false, float radiusSquared = 900.0f)
+        {
+            var peds = new List<int>();
+            int ped = 0;
+            int handle = API.FindFirstPed(ref ped);
+            var player = Game.Player.Character.Handle;
+            var coords = Game.Player.Character.Position;
+
+            if (handle == -1)
+                return peds;
+
+            do
+            {
+                var pos = new Ped(ped).Position;
+
+                if (!includeAnimals && !API.IsPedHuman(ped))
+                    continue;
+
+                if (!includePlayers && API.IsPedAPlayer(ped))
+                    continue;
+
+                if (!includeDead && API.IsPedDeadOrDying(ped, true))
+                    continue;
+
+                if (coords.DistanceToSquared(pos) > 900.0f || ped == player)
+                    continue;
+
+                peds.Add(ped);
+
+            } while (API.FindNextPed(handle, ref ped));
+
+            API.EndFindPed(handle);
+
+            return peds;
+        }
+
+        protected static List<int> GetCompanions(IEnumerable<int> peds)
+        {
+            var companions = new List<int>();
+
+            foreach (var ped in peds)
+            {
+                if (API.DecorGetBool(ped, PocceCompanionDecor) && API.NetworkHasControlOfEntity(ped))
+                    companions.Add(ped);
+
+                //API.DoesEntityBelongToThisScript(ped, )
+            }
+
+            return companions;
+        }
+
+        public static async Task RequestModel(uint model)
+        {
+            while (!API.HasModelLoaded(model))
+            {
+                API.RequestModel(model);
+                await Delay(10);
+            }
+        }
+
+        public static async Task RequestControl(int entity)
+        {
+            while (!API.NetworkHasControlOfEntity(entity))
+            {
+                API.NetworkRequestControlOfEntity(entity);
+                await Delay(10);
+            }
+        }
+
+        public static async Task<int> SpawnVehicle(string model)
+        {
+            var pos = Game.Player.Character.Position;
+            var hash = (uint)API.GetHashKey(model);
+
+            if (!API.IsModelValid(hash))
+            {
+                Notification(string.Format("Invalid model hash: 0x{0:X8} ({1})", hash, model));
+                return -1;
+            }
+
+            if (API.IsPedInAnyVehicle(Game.Player.Character.Handle, false))
+            {
+                Notification("Player is in a vehicle");
+                return -1;
+            }
+
+            await RequestModel(hash);
+            var vehicle = API.CreateVehicle(hash, pos.X, pos.Y, pos.Z + 1.0f, Game.Player.Character.Heading, true, false);
+            Game.Player.Character.SetIntoVehicle(new Vehicle(vehicle), VehicleSeat.Driver);
+            return vehicle;
+        }
+
+        public static async Task<int> SpawnProp(string model)
+        {
+            var player = Game.Player.Character.Handle;
+            var hash = (uint)API.GetHashKey(model);
+
+            if (!API.IsModelValid(hash))
+            {
+                Notification(string.Format("Invalid model hash: 0x{0:X8} ({1})", hash, model));
+                return -1;
+            }
+
+            await RequestModel(hash);
+            if (API.IsPedInAnyVehicle(player, false))
+            {
+                var vehicle = API.GetVehiclePedIsIn(player, false);
+                var roofBone = API.GetEntityBoneIndexByName(vehicle, "roof");
+                var pos = API.GetWorldPositionOfEntityBone(vehicle, roofBone);
+                var prop = API.CreateObject((int)hash, pos.X, pos.Y, pos.Z + 2.0f, true, false, true);
+                //API.AttachEntityToEntityPhysically(obj, vehicle, 0, roofBone, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1000.0f, true, false, true, true, 2);
+                API.AttachEntityToEntity(prop, vehicle, roofBone, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, false, false, false, false, 0, true);
+                return prop;
+            }
+            else
+            {
+                var pos = Game.Player.Character.Position;
+                var heading = API.GetEntityRotation(player, 0).Z;
+                var prop = API.CreateObject((int)hash, pos.X + (float)Math.Cos(heading), pos.Y + (float)Math.Sin(heading), pos.Z - 1.0f, true, false, true);
+                API.SetEntityCollision(prop, true, true);
+                return prop;
+            }
+        }
+
+        public static async Task<int> SpawnPed(uint[] modelList, int pedType = 26)
+        {
+            var model = modelList[API.GetRandomIntInRange(0, modelList.Length)];
+            var coords = Game.Player.Character.Position;
+            var pos = new Vector3();
+
+            if (!API.GetSafeCoordForPed(coords.X, coords.Y, coords.Z, true, ref pos, 16))
+            {
+                pos.X = coords.X;
+                pos.Y = coords.Y;
+                pos.Z = coords.Z + 1.0f;
+            }
+
+            await RequestModel(model);
+            return API.CreatePed(pedType, model, pos.X, pos.Y, pos.Z, 0.0f, true, false);
+        }
+
+        public static async Task ArmPed(int ped, uint[] weaponList)
+        {
+            API.TaskSetBlockingOfNonTemporaryEvents(ped, true);
+            API.SetPedKeepTask(ped, true);
+            await Delay(10);
+
+            API.SetPedCombatAbility(ped, 100);
+            API.SetPedCombatMovement(ped, 2);
+            API.SetPedCombatRange(ped, 2);
+            API.SetPedHearingRange(ped, float.MaxValue);
+            API.SetPedCombatAttributes(ped, 5, true);
+            API.SetPedCombatAttributes(ped, 17, true);
+            API.SetPedCombatAttributes(ped, 46, true);
+            API.SetPedCombatAttributes(ped, 1424, true);
+            API.SetPedFleeAttributes(ped, 0, false);
+            API.SetEntityHealth(ped, 200);
+
+            if (weaponList != null)
+            {
+                API.SetPedArmour(ped, 200);
+                var weapon = weaponList[API.GetRandomIntInRange(0, weaponList.Length)];
+                new Ped(ped).Weapons.Give((WeaponHash)weapon, int.MaxValue, true, true);
+            }
+        }
+
+        public static async Task MakeCompanion(int ped)
+        {
+            var player = Game.Player.Character.Handle;
+            var playerGroup = API.GetPedGroupIndex(player);
+
+            if (!API.DecorIsRegisteredAsType(PocceCompanionDecor, 2))
+                API.DecorRegister(PocceCompanionDecor, 2);
+
+            API.DecorSetBool(ped, PocceCompanionDecor, true);
+            API.SetPedRelationshipGroupHash(ped, (uint)API.GetPedRelationshipGroupHash(player));
+
+            API.ClearPedTasksImmediately(ped);
+            API.TaskSetBlockingOfNonTemporaryEvents(ped, true);
+            API.SetPedKeepTask(ped, true);
+            await Delay(10);
+
+            API.TaskGoToEntity(ped, player, -1, 5.0f, 2.0f, 0, 0);
+        }
+
+        public static async Task UpdateCompanions()
+        {
+            var player = Game.Player.Character.Handle;
+            var peds = GetPeds();
+            var companions = GetCompanions(peds);
+
+            if (companions.Count == 0)
+                return;
+
+            int target = 0;
+            if (API.GetEntityPlayerIsFreeAimingAt(API.GetPlayerIndex(), ref target) || API.GetPlayerTargetEntity(API.GetPlayerIndex(), ref target))
+            {
+                foreach (var companion in companions)
+                {
+                    API.TaskCombatPed(companion, target, 0, 16);
+                }
+                return;
+            }
+
+            foreach (var ped in peds)
+            {
+                if (API.IsPedInCombat(ped, player))
+                {
+                    if (API.DecorGetBool(ped, PocceCompanionDecor))
+                    {
+                        API.ClearPedTasks(ped);
+                        continue;
+                    }
+
+                    foreach (var companion in companions)
+                    {
+                        API.TaskCombatPed(companion, ped, 0, 16);
+                    }
+                    return;
+                }
+            }
+
+            if (API.IsPedInAnyVehicle(player, false))
+            {
+                var vehicle = API.GetVehiclePedIsIn(player, false);
+                foreach (var companion in companions)
+                {
+                    if (!API.IsPedHuman(companion))
+                        continue;
+
+                    if (!API.AreAnyVehicleSeatsFree(vehicle))
+                        break;
+
+                    if (API.IsPedInAnyVehicle(companion, true))
+                    {
+                        var otherVehicle = API.GetVehiclePedIsUsing(companion);
+                        if (otherVehicle != vehicle)
+                        {
+                            API.TaskLeaveVehicle(companion, otherVehicle, 0);
+                            await Delay(10);
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+
+                    var model = (uint)API.GetEntityModel(vehicle);
+                    int seats = API.GetVehicleModelNumberOfSeats(model);
+
+                    for (int seat = -1; seat < seats; ++seat)
+                    {
+                        if (API.IsVehicleSeatFree(vehicle, seat))
+                        {
+                            API.TaskEnterVehicle(companion, vehicle, -1, 0, 2.0f, 1, 0);
+                            continue;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                foreach (var companion in companions)
+                {
+                    var pos = API.GetEntityCoords(companion, true);
+                    if (API.IsPedInAnyVehicle(companion, true))
+                    {
+                        var vehicle = API.GetVehiclePedIsIn(companion, false);
+                        if (API.IsVehicleStopped(vehicle))
+                            API.TaskLeaveVehicle(companion, vehicle, 0);
+                        else
+                            API.TaskLeaveVehicle(companion, vehicle, 4096);
+                    }
+                    else if (pos.DistanceToSquared(Game.Player.Character.Position) > 25.0f)
+                    {
+                        API.TaskGoToEntity(companion, player, -1, 5.0f, 2.0f, 0, 0);
+                    }
+                    else if (API.IsPedHuman(companion))
+                    {
+                        if (!API.IsPedActiveInScenario(companion))
+                        {
+                            var heading = API.GetEntityHeading(companion);
+                            var scenario = ScenarioList[API.GetRandomIntInRange(0, ScenarioList.Length)];
+                            API.TaskLookAtEntity(companion, player, -1, 2048, 3);
+                            API.TaskStandGuard(companion, pos.X, pos.Y, pos.Z, heading, scenario);
+                        }
+                    }
+                    else
+                    {
+                        API.TaskLookAtEntity(companion, player, -1, 2048, 3);
+                    }
+                }
+            }
+        }
+    }
+}
