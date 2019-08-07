ed635f9ae6c93b42e02c9de38483ece194ff4134
Gábor Görzsöny <gabor@gorzsony.com>
2019-08-07 15:02:20 +0200

    Filter invalid props from config

--- Client/Menus/PropMenu.cs
+++ Client/Menus/PropMenu.cs
@@ -1,11 +1,32 @@
-﻿using PocceMod.Shared;
+﻿using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using PocceMod.Shared;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PocceMod.Client.Menus
 {
     public class PropMenu : Submenu
     {
-        public PropMenu() : base("select prop", Props.Spawn, Config.PropList, 10)
+        public PropMenu() : base("select prop", Props.Spawn, FilteredPropList(), 10)
         {
+        }
+
+        public static bool IsValidProp(string prop)
+        {
+            var model = (uint)API.GetHashKey(prop);
+            if (!API.IsModelValid(model))
+            {
+                Debug.WriteLine("[PocceMod] invalid prop: " + prop);
+                return false;
+            }
+
+            return true;
+        }
+
+        public static IEnumerable<string> FilteredPropList()
+        {
+            return Config.PropList.Where(prop => IsValidProp(prop));
         }
     }
 }
