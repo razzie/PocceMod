b6fbaea8b8e1c882c9404f8498a07b397fd1066c
Gábor Görzsöny <gabor@gorzsony.com>
2019-08-16 16:53:35 +0200

    Server side debugging for future use

--- Server/Main.cs
+++ Server/Main.cs
@@ -6,6 +6,7 @@ namespace PocceMod.Server
 {
     public class Main : BaseScript
     {
+        private const bool _debug = false;
         private readonly RopeSet _ropes = new RopeSet();
 
         public Main()
@@ -27,42 +28,64 @@ namespace PocceMod.Server
             EventHandlers["PocceMod:SetMPSkin"] += new Action<Player, int, dynamic, int>(SetMPSkin);
         }
 
+        private static void Debug(Player source, string text)
+        {
+#pragma warning disable CS0162
+            if (_debug)
+                CitizenFX.Core.Debug.WriteLine(string.Format("[player#{0}] {1}", source.Handle, text));
+#pragma warning restore CS0162
+        }
+
         private void PlayerDropped([FromSource] Player source, string reason)
         {
+            Debug(source, "PlayerDropped");
+
             ClearRopes(source);
         }
 
         private void Burn([FromSource] Player source, int entity)
         {
+            Debug(source, "Burn");
+
             if (Permission.CanDo(source, Ability.SpawnTrashPed))
                 TriggerClientEvent("PocceMod:Burn", entity);
         }
 
         private void EMP([FromSource] Player source, int vehicle)
         {
+            Debug(source, "EMP");
+
             if (Permission.CanDo(source, Ability.EMPOtherPlayer))
                 TriggerClientEvent("PocceMod:EMP", vehicle);
         }
 
         private void CompressVehicle([FromSource] Player source, int vehicle)
         {
+            Debug(source, "CompressVehicle");
+
             if (Permission.CanDo(source, Ability.CompressVehicle))
                 TriggerClientEvent("PocceMod:CompressVehicle", vehicle);
         }
 
         private void SetIndicator([FromSource] Player source, int vehicle, int state)
         {
+            Debug(source, "SetIndicator");
+
             TriggerClientEvent("PocceMod:SetIndicator", vehicle, state);
         }
 
         private void ToggleHorn([FromSource] Player source, int vehicle, bool state)
         {
+            Debug(source, "ToggleHorn");
+
             if (Permission.CanDo(source, Ability.AircraftHorn))
                 TriggerClientEvent("PocceMod:ToggleHorn", vehicle, state);
         }
 
         private void AddRope([FromSource] Player source, int entity1, int entity2, Vector3 offset1, Vector3 offset2, int mode)
         {
+            Debug(source, "AddRope");
+
             if (Permission.CanDo(source, Ability.Rope) || Permission.CanDo(source, Ability.RopeGun) || Permission.CanDo(source, Ability.Balloons))
             {
                 TriggerClientEvent("PocceMod:AddRope", source.Handle, entity1, entity2, offset1, offset2, (int)mode);
@@ -74,24 +97,32 @@ namespace PocceMod.Server
 
         private void ClearRopes([FromSource] Player source)
         {
+            Debug(source, "ClearRopes");
+
             TriggerClientEvent("PocceMod:ClearRopes", source.Handle);
             _ropes.ClearRopes(source);
         }
 
         private void ClearLastRope([FromSource] Player source)
         {
+            Debug(source, "ClearLastRope");
+
             TriggerClientEvent("PocceMod:ClearLastRope", source.Handle);
             _ropes.ClearLastRope(source);
         }
 
         private void ClearEntityRopes([FromSource] Player source, int entity)
         {
+            Debug(source, "ClearEntityRopes");
+
             TriggerClientEvent("PocceMod:ClearEntityRopes", entity);
             _ropes.ClearEntityRopes(entity);
         }
 
         private void RequestRopes([FromSource] Player source)
         {
+            Debug(source, "RequestRopes");
+
             foreach (var rope in _ropes.GetRopes())
             {
                 rope.Player.TriggerEvent("PocceMod:AddRope", rope.Player.Handle, rope.Entity1, rope.Entity2, rope.Offset1, rope.Offset2, rope.Mode);
@@ -100,23 +131,31 @@ namespace PocceMod.Server
 
         private void AntiGravityAdd([FromSource] Player source, int entity, float force)
         {
+            Debug(source, "AntiGravityAdd");
+
             if (Permission.CanDo(source, Ability.AntiGravity) || Permission.CanDo(source, Ability.Balloons))
                 TriggerClientEvent("PocceMod:AntiGravityAdd", entity, force);
         }
 
         private void AntiGravityRemove([FromSource] Player source, int entity)
         {
+            Debug(source, "AntiGravityRemove");
+
             if (Permission.CanDo(source, Ability.AntiGravity) || Permission.CanDo(source, Ability.Balloons))
                 TriggerClientEvent("PocceMod:AntiGravityRemove", entity);
         }
 
         private void RequestMPSkin([FromSource] Player source, int ped, int ownerPlayer)
         {
+            Debug(source, "RequestMPSkin");
+
             Players[ownerPlayer].TriggerEvent("PocceMod:RequestMPSkin", ped, source.Handle);
         }
 
         private void SetMPSkin([FromSource] Player source, int ped, dynamic skin, int requestingPlayer)
         {
+            Debug(source, "SetMPSkin");
+
             Players[requestingPlayer].TriggerEvent("PocceMod:SetMPSkin", ped, skin);
         }
     }
