b80e8f2eecec9e0e994fac00acdcc7aab68e107f
Gábor Görzsöny <gabor@gorzsony.com>
2020-04-03 00:42:32 +0200

    RopePool always tries to return a different rope each time

--- Client/RopePool.cs
+++ Client/RopePool.cs
@@ -1,14 +1,13 @@
 ﻿using CitizenFX.Core.Native;
 using PocceMod.Shared;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace PocceMod.Client
 {
     public static class RopePool
     {
         private static readonly int PoolSize = Config.GetConfigInt("RopePoolSize");
-        private static readonly HashSet<int> _pool = new HashSet<int>();
+        private static readonly Queue<int> _pool = new Queue<int>();
 
         static RopePool()
         {
@@ -16,15 +15,13 @@ namespace PocceMod.Client
             {
                 int unkPtr = 0;
                 int handle = API.AddRope(0f, 0f, 0f, 0f, 0f, 0f, Ropes.MaxLength, 1, Ropes.MaxLength, 0.25f, 0f, false, false, false, 5f, false, ref unkPtr);
-                _pool.Add(handle);
+                _pool.Enqueue(handle);
             }
         }
 
         public static int AddRope()
         {
-            var rope = _pool.First();
-            _pool.Remove(rope);
-            return rope;
+            return _pool.Dequeue();
         }
 
         public static void DeleteRope(ref int rope)
@@ -32,7 +29,7 @@ namespace PocceMod.Client
             API.StopRopeUnwindingFront(rope);
             API.StopRopeWinding(rope);
             API.RopeConvertToSimple(rope);
-            _pool.Add(rope);
+            _pool.Enqueue(rope);
             rope = -1;
         }
     }
